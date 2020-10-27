fe85e110e751aeb80b29cdc4eddcc2a1ff9bd1c5
yhetman <juliagetman5@gmail.com>
2020-10-27 22:58:31 +0200

    reading done

--- finite_auto.h
+++ finite_auto.h
@@ -12,7 +12,7 @@
 #ifndef FINITE_AUTO_H
 # define FINITE_AUTO_H
 
-
+# include <fcntl.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -21,20 +21,20 @@ typedef struct 	s_state
 {
 	char		*current_state;
 	char		*condition;
-	bool 		finish;
-	bool		start;
-	s_state		*transfer_state;
+	char		*transfer_state;
+	struct s_state		*next;
 }				t_state;
 
 typedef struct 	s_finite_auto
 {
+	char		*current_state;
 	char		**alphabet;
 	int			number_states;
-	char		**start_states;
+	char		*start_states;
 	char		**finish_states;
 	char		*w1;
 	char		*w2;
 	t_state 	*states;
 }				t_finite_auto;
 
-#endif FINITE_AUTO_H
+#endif
--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "finish_auto.h"
-#include "libft.h"
+#include "finite_auto.h"
+#include "libft/includes/libft.h"
+#include <fcntl.h>
 
 static t_state *malloc_states()
 {
@@ -28,28 +29,65 @@ static t_finite_auto 	*malloc_finite_auto()
 
 	machine = (t_finite_auto *)malloc(sizeof(t_finite_auto));
 	ft_bzero(machine, sizeof(t_finite_auto));
-	machine->states = malloc_states
-	return machine;
+	return (machine);
 }
+
+
+t_state *parse_line_to_state(char *line)
+{
+	t_state *state;
+	char **parts;
+
+	state = malloc_states();
+	parts = ft_strsplit(line, ' ');
+	state->current_state = parts[0];
+	state->condition = parts[1];
+	state->transfer_state = parts[2];
+	return (state);
+}
+
+int 	init_states(t_finite_auto	*machine, char		*line, int fd)
+{
+	int			i;
+	t_state	*new_state;
+
+	i = 0;
+	while(i < machine->number_states)
+	{
+		get_next_line(fd, &line);
+		if (new_state = parse_line_to_state(line))
+		{
+			new_state->next = machine->states;
+			machine->states = new_state;
+		}
+		i++;
+	}
+	return (1);
+}
+
 
 int 		reading_file(int fd)
 {
-	t_finite_auto	*machine
+	t_finite_auto	*machine;
 	char		*line;
 	int			i;
 
 	line = NULL;
-	machine = malloc_finite_auto()
+	machine = malloc_finite_auto();
 	if (get_next_line(fd, &line))
-		machine->alphabet = ft_strsplit(line, ' ')
+		machine->alphabet = ft_strsplit(line, ' ');
 	if (get_next_line(fd, &line))
 		machine->number_states = ft_atoi(line);
 	if (get_next_line(fd, &line))
-		machine->start_states = ft_strsplit(line, ' ');
+		machine->start_states = line;
+		machine->current_state = machine->start_states;
 	if (get_next_line(fd, &line))
 		machine->finish_states = ft_strsplit(line, ' ');
-	while(get_next_line(fd, &line))
-		machine->states = init_states(machine);
+	init_states(machine, line, fd);
+	printf("%s\n%s\n%s\n%d\n",
+		machine->alphabet[0], machine->start_states, machine->finish_states[0],machine->number_states);
+	printf("%s\n", machine->states->next->condition);
+	return (1);
 }
 
 
